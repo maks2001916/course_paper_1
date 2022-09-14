8145221c3cb65bc7bfcedcdbc2aba64197525295
maks <mamonov.maksim.20003@gmail.com>
2022-09-14 20:47:42 +0500

    доработал недочёты

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+
         Employee ivan = new Employee(new Personality("Ivan", "Ivanov", "Ivanovich"), "1", 57825);
         Employee sergei = new Employee(new Personality("Sergei", "Sergeev", "Sergeevich"), "2", 74625);
         Employee dmitrii = new Employee(new Personality("Dmitrii", "Dmitriev", "Dmitrievich"), "3", 63817);
@@ -20,6 +21,9 @@ public class Main {
         averageSalary(storage);
         fio(storage);
     }
+
+    public static Employee[] employees = new Employee[10];
+
     public static void list(Employee[] storage) {
         for (int i = 0; i < storage.length; i++) {
             System.out.println(storage[i]);
@@ -51,15 +55,17 @@ public class Main {
         System.out.println("Максимальная зарплата - " + x);
     }
     public static void averageSalary(Employee[] averageSalary) {
-        int x = 0;
-        for (int i = 0; i < averageSalary.length; i++) {
+        double x = 0;
+        double y = averageSalary.length;
+        for (int i = 0; i < y; i++) {
             x += averageSalary[i].getSalary();
         }
-        System.out.println("Средняя зарплата - " + x / averageSalary.length);
+        System.out.println("Средняя зарплата - " + x / y);
     }
     public static void fio(Employee[] fio) {
         for (int i = 0; i < fio.length; i++) {
             System.out.println(fio[i].getFio());
         }
     }
+
 }
